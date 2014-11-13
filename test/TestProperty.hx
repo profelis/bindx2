be2a4c6010b1585db194a261f43ec83ee596e686
Dima Granetchi <system.grand@gmail.com>
2014-11-13 21:56:39 +0200

    remove old tests

--- test/TestProperty.hx
+++ test/TestProperty.hx
@@ -257,41 +257,6 @@ class TestProperty extends BuddySuite {
 			});
 		});
 	}
-
-	/*function test1() {
-		var p = new BindableProperty();
-		p.s = "1";
-		var callNum = 0;
-
-		Bind.bind(p.s, function (from, to) {
-			assertEquals(from, "1");
-			assertEquals(to, "");
-			callNum ++;
-		});
-
-		p.s = null;
-
-        Bind.unbind(p.s);
-
-		Bind.bind(p.s, function (from, to) {
-			assertEquals(from, "");
-			assertEquals(to, "1");
-			callNum ++;
-		});
-		p.s = "1";
-
-		assertEquals(callNum, 2);
-	}
-	
-	function test2() {
-		var p = new BindableProperty();
-		var t = {a:""};
-		Bind.bindTo(p.s, t.a);
-		
-		p.s = "123";
-		assertEquals(t.a, p.s);
-	}
-	*/
 }
 
 class BindableProperty implements bindx.IBindable {
