42aabddec3703d69f3325f424645e509745eea9e
Dima Granetchi <system.grand@gmail.com>
2014-11-18 20:02:52 +0200

    chain bind WIP

--- test/Tests.hx
+++ test/Tests.hx
@@ -1,6 +1,25 @@
 package ;
 
 import buddy.BuddySuite;
+import buddy.SuitesRunner;
 
-@:build(buddy.GenerateMain.build(null, ["test"]))
-class Tests extends BuddySuite {}
+class Tests extends BuddySuite {
+    
+    public static function main() {
+        var reporter = new buddy.reporting.TravisHxReporter();
+
+        var runner = new SuitesRunner([
+            new BaseTest(),
+            new InheritanceTest(),
+            new InlineTest(),
+            new MetaTest(),
+            new SignalTest(),
+            new TestProperty(),
+            // new ChainBindTest(),
+        ], reporter);
+
+        runner.run();
+
+        return runner.statusCode();
+    }
+}
