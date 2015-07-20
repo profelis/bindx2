e972488062f7e18a8302bfce08e8155851aaf4ee
Dima Granetchi <system.grand@gmail.com>
2015-07-20 11:47:48 +0300

    buddy tests file fix

--- test/Tests.hx
+++ test/Tests.hx
@@ -1,16 +1,5 @@
 package ;
 
-import buddy.BuddySuite;
-import buddy.SuitesRunner;
+import buddy.Buddy;
 
-@:build(buddy.GenerateMain.withSuites([
-    new BaseTest(),
-    new InheritanceTest(),
-    new InlineTest(),
-    new MetaTest(),
-    new SignalTest(),
-    new TestProperty(),
-    new ChainBindTest(),
-    new ExprBindTest(),
-]))
-class Tests extends BuddySuite {}
+class Tests implements Buddy<[BaseTest, InheritanceTest, InlineTest, MetaTest, SignalTest, TestProperty, ChainBindTest, ExprBindTest]> {}
