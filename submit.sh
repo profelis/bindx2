2ca2c1f46e531a6b339ce700c5bfc1747ebb9674
Dima Granetchi <system.grand@gmail.com>
2017-03-12 05:34:04 +0200

    minor fix extra params

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -f haxelib.zip
-zip -r haxelib.zip src haxelib.json README.md LICENSE extraParams.hx
+zip -r haxelib.zip src haxelib.json README.md LICENSE extraParams.hxml
 haxelib submit haxelib.zip
